57f8087f5181f1891ab50bcdb83886ecaa8cfe22
Cynthia Rittenbach <cynthiarittenbach@Cynthias-iMac.local>
2021-10-15 11:08:46 -0400

    updated mouth component

--- src/components/Mouth.js
+++ src/components/Mouth.js
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 
 import defaultt from '/Users/cynthiarittenbach/alpaca/src/assets/images/mouth/default.png';
 import astonished from '/Users/cynthiarittenbach/alpaca/src/assets/images/mouth/astonished.png';
+import eating from '/Users/cynthiarittenbach/alpaca/src/assets/images/mouth/eating.png';
+import laugh from '/Users/cynthiarittenbach/alpaca/src/assets/images/mouth/laugh.png';
+import tongue from '/Users/cynthiarittenbach/alpaca/src/assets/images/mouth/tongue.png';
 
 
 const Mouth = (props) => {
@@ -15,6 +18,12 @@ const Mouth = (props) => {
              return <img src={defaultt} className="mouth"/>
              case 'astonished':
              return <img src={astonished} className="mouth"/>
+             case 'eating':
+             return <img src={eating} className="mouth"/>
+             case 'laugh':
+             return <img src={laugh} className="mouth"/>
+             case 'tongue':
+             return <img src={tongue} className="mouth"/>
            default:
              return null
          }

1ec9bdf4595ddce1b3d4fed91152e6e561b7fc6f
Cynthia Rittenbach <cynthiarittenbach@Cynthias-iMac.local>
2021-10-15 10:57:24 -0400

    updated eyes component

--- src/components/Eyes.js
+++ src/components/Eyes.js
@@ -4,6 +4,10 @@ import React, { useState } from 'react';
 
 import defaultt from '/Users/cynthiarittenbach/alpaca/src/assets/images/eyes/default.png';
 import angry from '/Users/cynthiarittenbach/alpaca/src/assets/images/eyes/angry.png';
+import naughty from '/Users/cynthiarittenbach/alpaca/src/assets/images/eyes/naughty.png';
+import panda from '/Users/cynthiarittenbach/alpaca/src/assets/images/eyes/panda.png';
+import smart from '/Users/cynthiarittenbach/alpaca/src/assets/images/eyes/smart.png';
+import star from '/Users/cynthiarittenbach/alpaca/src/assets/images/eyes/star.png';
 
 
 const Eyes = (props) => {
@@ -16,6 +20,14 @@ const Eyes = (props) => {
              return <img src={defaultt} className="eyes"/>
              case 'angry':
              return <img src={angry} className="eyes"/>
+             case 'naughty':
+             return <img src={naughty} className="eyes"/>
+             case 'panda':
+             return <img src={panda} className="eyes"/>
+             case 'smart':
+             return <img src={smart} className="eyes"/>
+             case 'star':
+             return <img src={star} className="eyes"/>
            default:
              return null
          }

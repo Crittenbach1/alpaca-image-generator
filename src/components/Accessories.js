0a7c40243c886e7d010e2d1f5809f8fba8296daf
Cynthia Rittenbach <cynthiarittenbach@Cynthias-iMac.local>
2021-10-15 10:48:04 -0400

    updated accessories component

--- src/components/Accessories.js
+++ src/components/Accessories.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import earings from '/Users/cynthiarittenbach/alpaca/src/assets/images/accessories/earings.png';
 import flower from '/Users/cynthiarittenbach/alpaca/src/assets/images/accessories/flower.png';
 
+import glasses from '/Users/cynthiarittenbach/alpaca/src/assets/images/accessories/glasses.png';
+import headphone from '/Users/cynthiarittenbach/alpaca/src/assets/images/accessories/headphone.png';
 
 const Accessories = (props) => {
     return (
@@ -14,6 +16,10 @@ const Accessories = (props) => {
              return <img src={earings} className="accessories"/>
              case 'flower':
              return <img src={flower} className="accessories"/>
+             case 'glasses':
+             return <img src={glasses} className="accessories"/>
+             case 'headphone':
+             return <img src={headphone} className="accessories"/>
            default:
              return null
          }
